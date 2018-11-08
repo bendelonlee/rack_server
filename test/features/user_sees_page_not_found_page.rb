8562958c26c7f8a4bba0894b9e08c02f4b1fa1d9
Ben Lee <bendelonlee@gmail.com>
2018-11-07 18:25:46 -0700

    Fixes typo where tests had the same name

--- test/features/user_sees_page_not_found_page.rb
+++ test/features/user_sees_page_not_found_page.rb
@@ -1,6 +1,6 @@
 require "./test/test_helper"
 
-class HomepageTest < CapybaraTestCase
+class ErrorpageTest < CapybaraTestCase
   def test_user_will_recieve_a_page_not_found_error_if_the_page_does_not_exist
     visit '/'
     assert page.has_content?("Page not found")
